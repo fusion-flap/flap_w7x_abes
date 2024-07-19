fd9e3b05e2b7ba1a3104ae23f7d600b152a91ef3
sandorzoletnik <zoletnik.sandor@ek-cer.hu>
2024-07-19 14:06:29 +0200

    ?

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .w7x_abes import *
 from .spatcal import *
-# from .bori_monitor_file_handling import read_data, find_files
+from .bori_monitor_file_handling import read_data, find_files
 # from .plot_beamdata import plot_beamdata
 
