4478d03027f78f6c56d568a1d78a95d27a4c9099
sandorzoletnik <zoletnik.sandor@ek-cer.hu>
2024-09-19 11:03:10 +0200

    init correct

--- __init__.py
+++ __init__.py
@@ -1,10 +1,6 @@
 from .w7x_abes import *
 from .spatcal import *
 from .cxrs import *
-<<<<<<< HEAD
 from .bori_monitor_file_handling import read_data, find_files#, read_exp_tdms
-=======
-from .bori_monitor_file_handling import read_data, find_files, read_exp_tdms
->>>>>>> ac0d459ed975f5d146e09e4a83b39619bb2bf4fd
 from .plot_beamdata import plot_beamdata
 
