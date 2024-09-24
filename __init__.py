0d88cfff47bba01f26ddf6351acb39c79aa4d1c2
APDCAM Measurement User <apdcam@sv-dia-qsi-3.ipp-hgw.mpg.de>
2024-09-24 10:53:45 +0200

    started cleaning up BORI log files

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
 from .w7x_abes import *
 from .spatcal import *
 from .cxrs import *
-from .bori_monitor_file_handling import read_data, find_files#, read_exp_tdms
-from .plot_beamdata import plot_beamdata
-
+from .bori_log import *
