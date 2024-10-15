176715b8235f67351fdcc00908ac4f6bf8c7d29f
sandorzoletnik <zoletnik.sandor@ek-cer.hu>
2024-10-15 11:22:00 +0200

    plot_chopper now really added to __init__

--- __init__.py
+++ __init__.py
@@ -5,3 +5,4 @@ from .cxrs_util import *
 from .bori_log import *
 from .exp_summary import exp_summary, exp_summaries
 from .tests.test_w7x_chopper import test_chopper_timing
+from .plot_chopper import plot_chopper
